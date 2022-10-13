104e5e4db20fb631d4b2cbae81c1270924172dcd
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2022-10-13 14:23:00 +0200

    Split tutorial 4 in different tasks

---
+++ o2at-1/o2at-h4-1-skimming.cxx
@@ -0,0 +1,84 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright
+// holders. All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+///
+/// \brief This task contains the individual steps that are to be taken
+///        in the first part of the tutorial. These are 6 steps, and at the end,
+///        the participant is expected to have a pT resolution histogram
+///        as a function of transverse momentum.
+/// \author
+/// \since
+
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/HistogramRegistry.h"
+#include "PWGHF/DataModel/HFSecondaryVertex.h"
+#include "PWGHF/DataModel/HFCandidateSelectionTables.h"
+
+using namespace o2;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+
+// STEP 1
+//<- starting point, define the derived table to be stored
+// this can be done in a separated header file, but for semplicity we do it in
+// the same file here
+
+namespace o2::aod
+{
+namespace mytable
+{
+DECLARE_SOA_COLUMN(InvMassD0, invMassD0, float);           //!
+DECLARE_SOA_COLUMN(InvMassD0bar, invMassD0bar, float);     //!
+DECLARE_SOA_COLUMN(Pt, pt, float);                         //!
+DECLARE_SOA_COLUMN(CosinePointing, cosinePointing, float); //!
+DECLARE_SOA_INDEX_COLUMN(Collision, collision);            //!
+} // namespace mytable
+
+DECLARE_SOA_TABLE(MyTable, "AOD", "MYTABLE", //!
+                  mytable::InvMassD0,
+                  mytable::InvMassD0bar,
+                  mytable::Pt,
+                  mytable::CosinePointing,
+                  mytable::CollisionId)
+
+} // namespace o2::aod
+
+struct ReadHFCandidates { //<- simple workflow that loops over HF 2-prong
+                          // candidates
+
+  void process(aod::HfCandProng2 const& cand2Prongs)
+  {
+
+    // loop over HF 2-prong candidates
+    for (auto& cand : cand2Prongs) {
+
+      // check first if the HF 2-prong candidate is tagged as a D0
+      bool isD0Sel = TESTBIT(cand.hfflag(), aod::hf_cand_prong2::DecayType::D0ToPiK);
+
+      if (!isD0Sel) {
+        continue;
+      }
+
+      auto invMassD0 = InvMassD0(cand);
+      auto invMassD0bar = InvMassD0bar(cand);
+
+      LOG(debug) << "Candidate with mass(D0) = " << invMassD0
+                 << ", mass(D0bar) = " << invMassD0bar
+                 << ", pt = " << cand.pt()
+                 << ", cos(theta_P) = " << cand.cpa();
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{adaptAnalysisTask<ReadHFCandidates>(cfgc)};
+}

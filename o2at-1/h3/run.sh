fe5ed6f7327738c1f8448524f47a1f920d2ad085
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2022-10-14 16:45:24 +0200

    Add files

---
+++ o2at-1/h3/run.sh
@@ -0,0 +1,3 @@
+export OPTIONS="-b --configuration json://config.json"
+
+o2-analysistutorial-h3-5-v0mcexample ${OPTIONS} | o2-analysis-timestamp ${OPTIONS} | o2-analysis-track-propagation ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-lf-lambdakzerobuilder ${OPTIONS} | o2-analysis-pid-tpc ${OPTIONS} | o2-analysis-multiplicity-table ${OPTIONS}

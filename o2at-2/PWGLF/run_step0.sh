335ac5dbbcbf3bfcd8065fbbba3fe372c908ef83
Nicolò Jacazio <njacazio@users.noreply.github.com>
2023-04-14 11:10:55 +0200

    Create run_step0.sh

---
+++ o2at-2/PWGLF/run_step0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+export OPTIONS="-b --configuration json://${PWD}/json_strangenesstutorial.json --resources-monitoring 2 --aod-memory-rate-limit 1000000000 --shm-segment-size 6000000000"
+o2-analysis-timestamp ${OPTIONS} \
+| o2-analysis-event-selection ${OPTIONS} \
+| o2-analysis-lf-lambdakzerobuilder ${OPTIONS} \
+| o2-analysistutorial-strangeness-step0 ${OPTIONS}

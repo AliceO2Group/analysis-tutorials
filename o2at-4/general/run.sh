79969906d31842ed5549f2c419ad19a8a564e6d7
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-10-11 14:10:35 +0200

    Cleaning

--- o2at-4/general/run.sh
+++
@@ -1,4 +0,0 @@
-OPTION="-b --configuration json://myConfig.json --pipeline track-propagation:4"
-
-o2-analysistutorial-mm-my-example-task ${OPTION} | o2-analysis-track-propagation ${OPTION} | o2-analysis-timestamp ${OPTION}
-
